9238cee074b23502c0b3f0f293360294b122fa94
stefanfoulis <stefan.foulis@gmail.com>
2011-10-25 00:22:37 +0200

    nicer api. first tests with flattening data

--- dploi_server/api_urls.py
+++ dploi_server/api_urls.py
@@ -1,9 +1,11 @@
 #-*- coding: utf-8 -*-
 from django.conf.urls.defaults import patterns, url
 from djangorestframework.views import ListOrCreateModelView, InstanceModelView
+from dploi_server.resources import ApplicationConfigResource
 from .resources import ApplicationResource
 
 urlpatterns = patterns('',
     url(r'^applications/$',          ListOrCreateModelView.as_view(resource=ApplicationResource), name='application-resource-root'),
     url(r'^applications/(?P<name>[a-z0-9_]+)/$', InstanceModelView.as_view(resource=ApplicationResource)),
+    url(r'^applications/(?P<name>[a-z0-9_]+)/config/$', InstanceModelView.as_view(resource=ApplicationConfigResource)),
 )
--- dploi_server/resources.py
+++ dploi_server/resources.py
@@ -1,5 +1,6 @@
 #-*- coding: utf-8 -*-
-from djangorestframework.resources import ModelResource
+from djangorestframework.resources import ModelResource, FormResource
+from dploi_server.models import PostgresInstance, LoadBalancer
 from .models import Realm, Application, Deployment
 
 
@@ -12,13 +13,50 @@ class ApplicationResource(ModelResource):
     model = Application
     fields = (
         'name', 'verbose_name', 'description', 'repository',
-        ('deployments',
-            ('identifier', 'is_live', 'name', 'branch',
-                ('domains', ('name',)),
-                ('redirect_domains', ('name',)),
-                ('postgres_instances', ('alias', 'name', 'user', 'password', 'service',)),
-            )
-        ),
+#        ('deployments',
+#            ('identifier', 'is_live', 'name', 'branch',
+#                ('domains', ('name',)),
+#                ('redirect_domains', ('name',)),
+#                ('postgres_instances', ('alias', 'name', 'user', 'password', 'service',)),
+#            )
+#        ),
+    )
+    ordering = ('name',)
+    depth = 1
+
+
+class PostgresDatabaseConfigResource(FormResource):
+    model = PostgresInstance
+    fields = ('alias', 'host', 'port', 'user', 'password')
+
+    def host(self, instance):
+        return instance.service.host.private_ipv4
+
+    def port(self, instance):
+        return instance.service.port
+
+class LoadBalancerConfigResource(FormResource):
+    model = LoadBalancer
+    fields = ('ipv4',)
+
+    def ipv4(self, instance):
+        return instance.host.public_ipv4
+
+
+class DeploymentConfigResource(FormResource):
+    model = Deployment
+    fields = (
+        'identifier', 'name', 'branch', 'is_live',
+        ('postgres_instances', PostgresDatabaseConfigResource),
+        ('load_balancer', LoadBalancerConfigResource)
+    )
+
+
+class ApplicationConfigResource(ModelResource):
+    model = Application
+    fields = (
+        'name', 'verbose_name', 'description', 'repository',
+        ('deployments', DeploymentConfigResource)
     )
     ordering = ('name',)
     depth = 1
